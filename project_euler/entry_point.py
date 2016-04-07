be9ee49d6e9796595b333077b9b913ad6594ad37
Shridhara Rangaraju <mrshridhara@outlook.com>
2016-04-07 23:33:47 +0530

    Initial commit.

---
+++ project_euler/entry_point.py
@@ -0,0 +1,29 @@
+"""
+Entry point of the application.
+"""
+
+options = [('1', 'Multiples of 3 and 5'),
+           ('2', 'Even Fibonacci Numbers'),
+           ('q', 'Quit')]
+
+while True:
+    print('\nChoose one of the below:')
+    for option, name in options:
+        print(option, ': ', name, sep = '')
+    choice = input('\nYour choice:')
+
+    if choice == '1':
+        import multiples_of_3_and_5
+        limit = int(input('Enter the limit: '))
+        multiples_of_3_and_5.run(limit)
+
+    elif choice == '2':
+        import even_fibonacci_numbers
+        limit = int(input('Enter the limit: '))
+        even_fibonacci_numbers.run(limit)
+
+    elif choice == 'q' or choice == 'Q':
+        break
+
+    else:
+        print('Invalid input!')
---
+++ project_euler/multiples_of_3_and_5.py
@@ -0,0 +1,22 @@
+"""
+Problem 1:
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+
+https://projecteuler.net/problem=1
+"""
+
+def get_multiples_of_3_or_5(limit):
+    for i in range(limit):
+        if i % 3 == 0 or i % 5 == 0:
+            yield i
+
+def run(limit):
+    print('Sum of all the multiples of 3 or 5 below ' + str(limit) + ':')
+    print(sum(get_multiples_of_3_or_5(limit)))
+
+if __name__ == '__main__':
+    run(1000)
