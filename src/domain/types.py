e1e5c4da464cd415a4b688efef7019b7a9189329
Shridhara Rangaraju <mrshridhara@outlook.com>
2021-06-29 08:14:51 +0530

    Moved problem_groups into domain

--- src/domain/types.py
+++ src/domain/types.py
@@ -1,4 +1,10 @@
-from typing import Text
+"""
+Defines the domain types useful in discovring and registering problems.
+"""
+
+from .codekata import supermarket_pricing
+from .project_euler import (even_fibonacci_numbers, largest_palindrome_product,
+                            largest_prime_factor, multiples_of_3_and_5)
 
 
 class ProblemGroup:
@@ -15,3 +21,17 @@ class ProblemStatement:
     def __init__(self, name, description):
         self.name = name
         self.description = description
+
+
+problem_groups = {
+    "project_euler":
+    ProblemGroup("Project Euler")
+        .append("multiples_of_3_and_5", ProblemStatement("Multiples of 3 and 5", multiples_of_3_and_5.__doc__))
+        .append("even_fibonacci_numbers", ProblemStatement("Even Fibonacci Numbers", even_fibonacci_numbers.__doc__))
+        .append("largest_prime_factor", ProblemStatement("Largest Prime Factor", largest_prime_factor.__doc__))
+        .append("largest_palindrome_product", ProblemStatement("Largest Palindrome Product", largest_palindrome_product.__doc__)),
+
+    "codekata":
+    ProblemGroup("CodeKata")
+        .append("supermarket_pricing", ProblemStatement("Supermarket Pricing", supermarket_pricing.__doc__)),
+}
